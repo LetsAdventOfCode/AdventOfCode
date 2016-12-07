1fa2183c6584c9f7b7ea195e711f036110d68b4b
bambi <christopher.vigren@gmail.com>
2016-12-07 09:50:35 +0100

    updated .gitignore with .DS_Store and added bambi-dir

---
+++ bambi/01/index.js
@@ -0,0 +1,29 @@
+var d = 1; // north, east, south, west
+var x = 0, y = 0, ix = undefined, iy;
+
+var c = {};
+
+var input = 'R2, L3, R2, R4, L2, L1, R2, R4, R1, L4, L5, R5, R5, R2, R2, R1, L2, L3, L2, L1, R3, L5, R187, R1, R4, L1, R5, L3, L4, R50, L4, R2, R70, L3, L2, R4, R3, R194, L3, L4, L4, L3, L4, R4, R5, L1, L5, L4, R1, L2, R4, L5, L3, R4, L5, L5, R5, R3, R5, L2, L4, R4, L1, R3, R1, L1, L2, R2, R2, L3, R3, R2, R5, R2, R5, L3, R2, L5, R1, R2, R2, L4, L5, L1, L4, R4, R3, R1, R2, L1, L2, R4, R5, L2, R3, L4, L5, L5, L4, R4, L2, R1, R1, L2, L3, L2, R2, L4, R3, R2, L1, L3, L2, L4, L4, R2, L3, L3, R2, L4, L3, R4, R3, L2, L1, L4, R4, R2, L4, L4, L5, L1, R2, L5, L2, L3, R2, L2';
+
+var inst = input.toUpperCase().split(', ');
+
+for (var i = 0; i < inst.length; i++) {
+  var x2=x, y2=y;
+  inst[i][0] == 'R' ? d++ : d--;
+  d == 5 ? d = 1 : d == 0 ? d = 4 : true;
+  for (var r = 0; r < parseInt(inst[i].substring(1)); r++) {
+    c[y+'-'+x] = 1;
+    if (d % 2) {
+      y += (d == 1 ? -1 : 1);
+    } else {
+      x += (d == 2 ? 1 : -1);
+    }
+    if (c[y+'-'+x] == 1 && ix === undefined) { ix = x; iy = y; }
+  }
+}
+
+console.log(y, x);
+console.log(iy, ix);
+var a = (x < 0 ? -x : x) + (y < 0 ? -y : y);
+var ia = (ix < 0 ? -ix : ix) + (iy < 0 ? -iy : iy);
+console.log(a, ia);
