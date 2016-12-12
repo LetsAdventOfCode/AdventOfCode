481460f3d0e0a4b8c025f5a545192b0206defbb6
Kalle <karl.niklasson@lineducation.se>
2016-12-12 08:11:08 +0100

    solution for day 2 1 and day 7 1

--- kalle/2 1-2 js/december2.js
+++ kalle/2 1-2 js/december2.js
@@ -1,7 +1,15 @@
-
 function solve() {
     var input = document.getElementById("input").value;
-
-  
-    document.getElementById("solution").innerHTML = password;
+    var position = 5;
+    var codes = input.split("\n");
+    var keyPad = [[]];
+    var code = "";
+    for (var i = 0; i < codes.length; i++) {
+        for (var j = 0; j < codes[i].length; j++) {
+            position += codes[i][j] === "L" && position - 1 % 3 !== 0 ? -1 : codes[i][j] === "R" && 1 + position % 3 !== 1 ? 1 : 0;
+            position += codes[i][j] === "D" && position + 3 <= 9 ? 3 : codes[i][j] === "U" && position - 3 > 0 ? -3 : 0;
+        }
+        code += position;
+    }
+    document.getElementById("solution").innerHTML = code;
 }
--- kalle/7 1-2 js/december7.js
+++ kalle/7 1-2 js/december7.js
@@ -1,7 +1,52 @@
-
 function solve() {
     var input = document.getElementById("input").value;
+    var ips = input.split("\n");
+    var tlsIps = [];
+    for (var i = 0; i < ips.length; i++) {
+        var parsedInput = ips[i].split("[");
+        var hypernets = [];
+        var candidates = [];
+        candidates.push(parsedInput[0]);
+        for (var j = 1; j < parsedInput.length; j++) {
+            var a = parsedInput[j].split("]");
+            hypernets.push(a[0]);
+            candidates.push(a[1]);
+        }
+        var hypernetAbba = false;
+        for (var j = 0; j < hypernets.length; j++) {
+            if (hasAbba(hypernets[j])) {
+                hypernetAbba = true;
+                break;
+            }
+        }
+        if (hypernetAbba) {
+            continue;
+        }
+        for (var j = 0; j < candidates.length; j++) {
+            if (hasAbba(candidates[j])) {
+                tlsIps.push(ips[i])
+                break;
+            }
+        }
+    }
 
-  
-    document.getElementById("solution").innerHTML = password;
+    document.getElementById("solution").innerHTML = tlsIps.length;
+}
+
+function hasAbba(candidate) {
+    for (var i = 0; i < candidate.length - 3; i++) {
+        var p = candidate.substring(i, i + 4);
+        if (p[0] !== p[1] && isPalindrom(p)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isPalindrom(s) {
+    if (s === "")
+        return true;
+    if (s.length === 1)
+        return true;
+    return s[0] === s[s.length - 1] && isPalindrom(s.substring(1, s.length - 1));
 }
